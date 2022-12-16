c3c77c05e6fe6942f0416744d4fa9a985bc75d12
Radek Svoboda <radek.svoboda@vsb.cz>
2022-12-16 14:03:19 +0100

    Smart harbour page added

--- vsb-ai/src/Common/Footer.jsx
+++ vsb-ai/src/Common/Footer.jsx
@@ -10,7 +10,7 @@ export const Footer = (props) => {
     return (
         <footer className="footer font-small">
             <div className="container footer-copyright text-center py-3">© {year} Copyright:
-                <a href="https://ai.vsb.cz/"> ai.vsb.cz</a> <br/> Created by <a href='https://github.com/rasvob' rel="noopener noreferrer" target="_blank" className={`link ${'link-secondary'}`}>Radek Svoboda</a> with React ✌️
+                <a href="https://ai.vsb.cz/"> ai.vsb.cz</a> <br/> Created by <a href='https://github.com/rasvob' rel="noopener noreferrer" target="_blank" className={`link ${'link-secondary'}`}>Radek Svoboda</a> in <a href='https://reactjs.org/' rel="noopener noreferrer" target="_blank" className={`link ${'link-secondary'}`}>React</a> ✌️
             </div>
         </footer>
     )
--- vsb-ai/src/Pages/SmartHarbour.jsx
+++ vsb-ai/src/Pages/SmartHarbour.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 // eslint-disable-next-line 
 import { PageHero } from "../Common/PageHero";
 import Section from '../Common/Section';
-import { Accordion, Card, Button } from 'react-bootstrap';
+import { Accordion, Card, Button, Dropdown, DropdownButton, ButtonGroup, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import ReactPlayer from 'react-player'
 
 export const SmartHarbour = (props) => {
     const [tags] = useState(['Computer vision', 'Deep learning', 'Object tracking', 'Text detection', 'Optical character recognition', 'Video processing', 'Multimedial data']);
@@ -13,14 +14,14 @@ export const SmartHarbour = (props) => {
 
             <main className='container'>
                 <Section heading="What is it all about?">
-                    <p>Are you drowning in a sea of video footage without a boat in sight? Let our object detection and text detection algorithms rescue you! With our expert technology, we can quickly and easily detect and track those elusive boats, even in the choppiest of waters. And once we've spotted those boats, our OCR technology kicks in to extract the all-important licence plate numbers. No more manual searching, no more squinting at the screen - just efficient, automated boat-spotting. So why wait? Join our team and help us make this project a reality. And remember, a smart harbour is a happy harbour - so don't be a boat-dummy, use our technology!</p>
+                    <p>Are you drowning in a sea of video footage without a boat in sight? Let our object detection and text detection algorithms rescue you! With our expert technology, we can quickly and easily detect and track those elusive boats, even in the choppiest of waters <span role="img" aria-label='ship'>⛵</span>. And once we've spotted those boats, our OCR technology kicks in to extract the all-important licence plate numbers. No more manual searching, no more squinting at the screen - just efficient, automated boat-spotting. So why wait? Join our team and help us make this project a reality. And remember, a smart harbour is a happy harbour - so don't be a boat-dummy, use our technology!</p>
                 </Section>
 
                 <Section heading="Who are we looking for?">
-                    <p>We're working on a cool project that uses technology to automate the operation of a recreational harbour in the Czech Republic. We're looking for talented students who are interested in computer vision, machine learning, or related fields to help us with the tasks involved in this project. If you're looking for a cool project to work on for your thesis or just want to get some hands-on experience, we'd love to hear from you.</p>
+                    <p>We're working on a cool project that uses technology to automate the operation of a recreational harbour in the Czech Republic. We're looking for talented students <span role="img" aria-label='student'>🎓</span> who are interested in computer vision, machine learning, or related fields to help us with the tasks involved in this project. If you're looking for a cool project to work on for your thesis or just want to get some hands-on experience, we'd love to hear from you.</p>
                 </Section>
 
-                <Section heading="What are the tasks?">
+                <Section heading="What are the tasks you can work on?">
                     <p>
                     The project involves several tasks related to the automation of the operation of a recreational harbour. These tasks include:
                     </p>
@@ -40,7 +41,7 @@ export const SmartHarbour = (props) => {
                     <p>The project “Smart harbour” aims to leverage technology to automate the operation of a recreational harbour in the Czech Republic. The main goals of the project are to monitor the harbour occupancy and to evaluate and register vessels entering the harbour. To achieve these goals, the project is divided into three main parts. The first part focuses on scene change detection, using existing or developed approaches to identify whether a boat is present in an image. The second part focuses on object detection and tracking, using computer vision algorithms to identify and track the movements of boats in the harbour. The final part focuses on text detection and OCR, using machine learning algorithms to extract the “licence plate numbers” of boats in the harbour. </p>
                 </Section>
 
-                <div className='py-6'>
+                <div className='mb-3'>
                 <Accordion defaultActiveKey="-1">
                 <Card>
                     <Card.Header>
@@ -84,17 +85,89 @@ export const SmartHarbour = (props) => {
                 </Accordion>
                 </div>
 
-                <Section heading="Ha! I bet we've got your attention!">
+                <Section heading="Example of the available data">
+                    <div>
+                    <p>
+                        We have setup of two IP cameras at two different locations installed at the harbour right now. The two cameras aquiered data for several month, the video sequences are divided into the 10 minutes long chunks.
+                    </p>
+
+                    <p>
+                        The video sequences use FullHD resolution at 4fps framerate. Every video is HEVC encoded, 10 minutes of video is approx. 400 MB of data.
+                    </p>
+
+                    <p>
+                        We provide two 10 minutes long video samples from each camera below, you can download 2 hours of example data using the download button below. We can share much more data, just send us an <Link to="/contact">email <span role="img" aria-label='mail'>📧</span></Link>.
+                    </p>
+
+                    <Row>
+                        <Col md={6} sm={12}>
+                        <h4>Camera 1 (<span role="img" aria-label='boat'>🚢</span> 00:15)</h4>
+                        <ReactPlayer 
+                        controls="true" 
+                        volume={0.5}
+                        width="100%"
+                        height="100%"
+                        config={
+                            {
+                                forceVideo: true
+                            }
+                        }
+                        url='/vsbai/cfg_raw_cam_01_fhd_h265_20220827T144002.mkv' />
+                        </Col>
+
+                        <Col md={6} sm={12} className="mt-5 mt-md-0">
+                        <h4>Camera 2 (<span role="img" aria-label='boat'>🚢</span> 00:45)</h4>
+                        <ReactPlayer 
+                        controls="true" 
+                        volume={0.5}
+                        width="100%"
+                        height="100%"
+                        config={
+                            {
+                                forceVideo: true
+                            }
+                        }
+                        url='/vsbai/cfg_raw_cam_02_fhd_h265_20220827T144003.mkv' />
+                        </Col>
+                    </Row>
+                    </div>
+                </Section>
+                
+                
+                <div className='mt-5 mb-1'>
+                    <DropdownButton
+                        as={ButtonGroup}
+                        key="primary"
+                        variant="primary"
+                        title="Download the sample data"
+                        drop="right"
+                    >
+                        <Dropdown.Item eventKey="1" href="/vsbai/cfg_raw_cam_01_fhd_h265_20220827T144002.mkv" download>Camera 1 - 10 minutes example (363 MB)</Dropdown.Item>
+                        <Dropdown.Item eventKey="2" href="/vsbai/cfg_raw_cam_02_fhd_h265_20220827T144003.mkv" download>Camera 2 - 10 minutes example (577 MB)</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item eventKey="3" href="/vsbai/raw_cam_01_fhd_h265_20220827T130001.zip" download>Camera 1 - 1 hour of data (27. 08. 2022 13:00 - 14:00) (1.85 GB)</Dropdown.Item>
+                        <Dropdown.Item eventKey="4" href="/vsbai/raw_cam_01_fhd_h265_20220827T140001.zip" download>Camera 1 - 1 hour of data (27. 08. 2022 14:00 - 15:00) (2.14 GB)</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item eventKey="5" href="/vsbai/raw_cam_02_fhd_h265_20220827T130000.zip" download>Camera 2 - 1 hour of data (27. 08. 2022 13:00 - 14:00) (2.89 GB)</Dropdown.Item>
+                        <Dropdown.Item eventKey="6" href="/vsbai/raw_cam_02_fhd_h265_20220827T140000.zip" download>Camera 2 - 1 hour of data (27. 08. 2022 14:00 - 15:00) (2.38 GB)</Dropdown.Item>
+                    </DropdownButton>
+                </div>
+
+                <Section heading="Ha! I bet we've got you curious!">
                 <p>Are you ready to embark on a wild adventure with the machine learning and computer vision algorithms on the market? If so, give us a shout! We're always looking for new ports to explore and new ships to add to our fleet. </p>
 
-                {/* <a href="mailto:jan.platos@vsb.cz"></a> */}
-                {/* <a href="mailto:jan.platos@vsb.cz">Jan Platoš</a>, <a href="mailto:radek.svoboda@vsb.cz">Radek Svoboda</a> or <a href="mailto:petr.prokop@vsb.cz@vsb.cz">Petr Prokop</a> */}
                 <p>
-                If you want to get more information or want to start a collaboration, do not hesitate to <Link to="/contact">contact us </Link>.
+                If you want to get more information or want to start a collaboration, do not hesitate to <Link to="/contact">contact us <span role="img" aria-label='mail'>📧</span></Link>.
                 </p>
 
-                <p>Together, we can make waves in the world of automation and set sail towards a brighter future!</p>
+                <p>Together, we can make waves <span role="img" aria-label='wave'>🌊</span> in the world of automation and set sail towards a brighter future!</p>
                 </Section>
+
+                <div>
+                    <p className='text-muted'>
+                        <em><strong>Fun fact:</strong> Majority of the text content was generated using <a href='https://chat.openai.com/' rel="noopener noreferrer" target="_blank">ChatGPT</a>. If you love the AI as we do, definitely check it out!</em> <span role="img" aria-label='smile'>😊</span>
+                    </p>
+                </div>
             </main>
         </div>
     );
